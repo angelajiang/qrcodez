abe37f8fdd4873449b2a5f55816a2411774dd28c
Jeanette Huang <jeanette.y.huang@gmail.com>
2012-11-21 10:08:26 -0600

    removed netid from form

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,5 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :netid
 
   validates :name, :presence => true
-  validates :netid, :presence => true
   validates :email, :presence => true
 end
